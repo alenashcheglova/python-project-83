d571c29658bf14df6fcf4f28f9035ca7fb5d5a61
Alena Shcheglova <alenn521@icloud.com>
2026-08-13 02:05:45 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 . $HOME/.local/bin/env
-make install && psql -a -d "$DATABASE_URL" -f database.sql
+
+make install
+
+if [ -z "$DATABASE_URL" ]; then
+  echo "ERROR: DATABASE_URL is not available during build!"
+  exit 1
+fi
+
+psql -a -d "$DATABASE_URL" -f database.sql
